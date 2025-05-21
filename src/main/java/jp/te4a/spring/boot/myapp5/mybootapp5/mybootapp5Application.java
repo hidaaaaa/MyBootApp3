26cd4b64c828e9c773186531e4f7a4d8240ffd43
hidaaaa <te2a220637@gmail.com>
2025-05-21 12:17:55 +0900

    kadai5 commit

--- src/main/java/jp/te4a/spring/boot/myapp5/mybootapp5/mybootapp5Application.java
+++ src/main/java/jp/te4a/spring/boot/myapp5/mybootapp5/mybootapp5Application.java
@@ -1,13 +1,14 @@
-package com.example.demo;
+package jp.te4a.spring.boot.myapp5.mybootapp5;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class mybootapp5Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(mybootapp5Application.class, args);
 	}
 
 }
